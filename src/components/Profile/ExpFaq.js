8debe40a468e5383a7e47048b3515b77ec521bf0
Stanislav Oles  [garage] <zdreni@gmail.com>
2022-08-06 21:43:04 +0300

    Localization fixes

--- src/components/Profile/ExpFaq.js
+++ src/components/Profile/ExpFaq.js
@@ -8,34 +8,8 @@ export default function ExpFaq() {
   const lang = useLanguage();
   return (
     <div className="faq-exp">
-{lang.NEVER === "ніколи" &&
-      <div className="faq-header">
-        <strong>Досвід</strong> гравця засновується на кількості зіграних чартів.
-        <br />
-        Чим вище рівень чарту і краще оцінка на ньому, тем більше досвіду він дає.
-        <br />
-        Повторні спроби на тих же чартах не дають додатковий досвід. Щоб піднімати свій рівень, грай
-        нові треки та чарти.
-      </div>}
-
-{lang.NEVER === "никогда" &&
-      <div className="faq-header">
-        <strong>Опыт</strong> игрока основывается на количестве сыгранных чартов.
-        <br />
-        Чем выше уровень чарта и чем лучше оценка на нём, тем больше опыта он даёт.
-        <br />
-        Повторные попытки на тех же чартах не дают больше опыта. Чтобы поднимать свой уровень, играй
-        новые треки и чарты.
-      </div>
-}
-
-{lang.NEVER === "ніколи" &&
-      <div className="faq-header">Список рівнів та необхідний досвій для їх отримання:</div>
-}
-
-{lang.NEVER === "никогда" &&
-      <div className="faq-header">Список уровней и необходимый опыт для их получения:</div>
-}
+      {lang.EXP_FAQ}
+      {lang.EXP_TITLES_LIST_HEADER}
       <div className="ranks-list">
         {ranks.map(rank => (
           <div key={rank.threshold} className="rank">
