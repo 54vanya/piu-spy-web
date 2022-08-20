7ff13e4e9b07494a093c053e6fd5d21793bacaef
Ivan Anisimov <54vanya@gmail.com>
2022-08-20 23:45:34 +0300

    removed moment date convertation

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,9 +1,3 @@
-import moment from 'moment';
-
 export const parseDate = (textDate) => {
-  if (typeof textDate === 'string') {
-    return moment(textDate).toDate();
-  } else {
     return new Date(textDate);
-  }
 };
