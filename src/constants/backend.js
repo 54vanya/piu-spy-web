3f358a7b0e435575950554943b86d70bb8d0ec6c
Stanislav Oles <zdreni@gmail.com>
2022-08-30 21:38:33 +0300

    Backend domain changed

--- src/constants/backend.js
+++ src/constants/backend.js
@@ -1,3 +1,2 @@
-// export const HOST = 'http://localhost:5000';
-export const HOST = 'https://vps720616.ovh.net:5000';
+export const HOST = 'https://api.pumpking.top';
 export const SOCKET_SERVER_IP = 'ws://127.0.0.1:6789';
